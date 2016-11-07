d632024734a58582732c29182ee0ce95aa913513
thrilok209 <thrilok2000@gmail.com>
2016-11-07 10:30:03 +0530

    change home button to logo button

--- js/app.js
+++ js/app.js
@@ -68,11 +68,11 @@ var dateOfJounery;
         // var cityName = $('#Cityname').val();
         console.log(dateOfJounery);
         console.log(fromCityName);
-      var that = this;
-      var urlReq= "http://developer.goibibo.com/api/bus/search/?app_id=11201a52&app_key=7e43676620f2d1a98d7f42c59e1b080a&source="+toCityName+"&destination="+fromCityName+"&dateofdeparture="+dateOfJounery;
-      // var urlReq = 'http://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=2fefb549a19a1115cae6f75ba75d652e';
-      $.ajax({
-        url: urlReq,
+        var that = this;
+        var urlReq= "http://developer.goibibo.com/api/bus/search/?app_id=11201a52&app_key=7e43676620f2d1a98d7f42c59e1b080a&source="+toCityName+"&destination="+fromCityName+"&dateofdeparture="+dateOfJounery;
+        // var urlReq = 'http://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=2fefb549a19a1115cae6f75ba75d652e';
+        $.ajax({
+          url: urlReq,
 
 
           error: function() {
@@ -95,51 +95,51 @@ var dateOfJounery;
             }
             $('#YOURWISH').removeClass('hide');
 
-           for(i = 0;i<data.data.onwardflights.length;i++){
-             busName[i] = data.data.onwardflights[i].BusType;
-             duration[i] = data.data.onwardflights[i].duration;
-             depTime[i] = data.data.onwardflights[i].DepartureTime;
-             busFrom[i] =data.data.onwardflights[i].ServiceName;
-             busFare[i] = data.data.onwardflights[i].fare.totalfare;
-             busfeat[i] = data.data.onwardflights[i].amenities;
-             CompName[i] = data.data.onwardflights[i].TravelsName;
-             arrTime[i] = data.data.onwardflights[i].arrdate;
-             FullBusName[i] = data.data.onwardflights[i].ugcid;
-             seatAvi[i] = data.data.onwardflights[i].RouteSeatTypeDetail.list[0].SeatsAvailable;
-             var u = i+1;
-             $('#nameforfrom').html(busFrom[0].toUpperCase());
-             var innerHtml = '<tr data-bus="'+(i)+'"><td>'+u+'</td><td><a href="'+(u)+'busname">'+busName[i]+" "+CompName[i]+'</td><td>'+depTime[i]+'</td><td>'+duration[i]+'</td><td>'+"Rs."+busFare[i]+'</td><td id="seatschange">'+seatAvi[i]+'</td></tr>';
-             if(seatAvi[i]<=5){
-               $("#seatschange").css("color","red");
-             }
-             if(seatAvi[i]>5){
-               $("#seatschange").css("background-color","green");
-             }
-             $('tbody').append(innerHtml);
-             $('#detTable').removeClass('hide');
+            for(i = 0;i<data.data.onwardflights.length;i++){
+              busName[i] = data.data.onwardflights[i].BusType;
+              duration[i] = data.data.onwardflights[i].duration;
+              depTime[i] = data.data.onwardflights[i].DepartureTime;
+              busFrom[i] =data.data.onwardflights[i].ServiceName;
+              busFare[i] = data.data.onwardflights[i].fare.totalfare;
+              busfeat[i] = data.data.onwardflights[i].amenities;
+              CompName[i] = data.data.onwardflights[i].TravelsName;
+              arrTime[i] = data.data.onwardflights[i].arrdate;
+              FullBusName[i] = data.data.onwardflights[i].ugcid;
+              seatAvi[i] = data.data.onwardflights[i].RouteSeatTypeDetail.list[0].SeatsAvailable;
+              var u = i+1;
+              $('#nameforfrom').html(busFrom[0].toUpperCase() );
+              var innerHtml = '<tr data-bus="'+(i)+'"><td>'+u+'</td><td><a href="'+(u)+'busname">'+busName[i]+" "+CompName[i]+'</td><td>'+depTime[i]+'</td><td>'+duration[i]+'</td><td>'+"Rs."+busFare[i]+'</td><td id="seatschange">'+seatAvi[i]+'</td></tr>';
+              if(seatAvi[i]<=5){
+                $("#seatschange").css("color","red");
+              }
+              if(seatAvi[i]>5){
+                $("#seatschange").css("background-color","green");
+              }
+              $('tbody').append(innerHtml);
+              $('#detTable').removeClass('hide');
 
 
-           }
-          //  console.log(busName);
+            }
+            //  console.log(busName);
 
-          $('tr').click(function(ev) {
-            $('#detTable').addClass('hide');
-            ev.preventDefault();
-            var countryCode= this.dataset.bus;
-            console.log(countryCode);
-            var innerHtml = '<h2 id="FULLBUSNAME">'+FullBusName[(countryCode)]+'.</h2><p id="FROMCITY">Route: '+busFrom[countryCode].toUpperCase()+'</p><p id="DURATION">Time duration: '+duration[countryCode]+'</p><p id="DEPTIME">Departure Time: '+depTime[countryCode]+'.(Note: Reach the place 30Min before)</p><p id="ARRTIME">Arrival Time: '+arrTime[countryCode]+'.</p><p id="FARE"> Total Fare: Rs.'+busFare[countryCode]+'</p><p id="FEAT">The Amenity Available: '+busfeat[countryCode]+'</p><p id="BUSNAME">Type Of Bus: '+busName[countryCode]+'.</p><p id="SEAT">Number Of Seats Left Are : '+seatAvi[countryCode]+'.</p>';
-            $('#fullDetOfTheBus ').append(innerHtml);
-          })
-          $('#SEARCHANOTHER').click(function() {
+            $('tr').click(function(ev) {
+              $('#detTable').addClass('hide');
+              ev.preventDefault();
+              var countryCode= this.dataset.bus;
+              console.log(countryCode);
+              var innerHtml = '<h2 id="FULLBUSNAME">'+FullBusName[(countryCode)]+'.</h2><p id="FROMCITY">Route: '+busFrom[countryCode].toUpperCase()+'</p><p id="DURATION">Time duration: '+duration[countryCode]+'</p><p id="DEPTIME">Departure Time: '+depTime[countryCode]+'.(Note: Reach the place 30Min before)</p><p id="ARRTIME">Arrival Time: '+arrTime[countryCode]+'.</p><p id="FARE"> Total Fare: Rs.'+busFare[countryCode]+'</p><p id="FEAT">The Amenity Available: '+busfeat[countryCode]+'</p><p id="BUSNAME">Type Of Bus: '+busName[countryCode]+'.</p><p id="SEAT">Number Of Seats Left Are : '+seatAvi[countryCode]+'.</p>';
+              $('#fullDetOfTheBus ').append(innerHtml);
+            })
+            $('#logoReload').click(function() {
               location.reload();
-          });
+            });
 
 
 
 
           },
         });
-    });
+      });
     },
     searchBus: function(countryCode) {
 
@@ -157,10 +157,10 @@ var dateOfJounery;
     dataFetching: function () {
       var busName = data;    }
 
-  };
+    };
 
-  $(document).ready(function() {
-    CountryComparer.init();
-  });
+    $(document).ready(function() {
+      CountryComparer.init();
+    });
 
-})();
+  })();
